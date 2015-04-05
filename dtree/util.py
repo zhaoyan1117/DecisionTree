aafdcac19b630a727bb8188408573fcfad942b04
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-04 22:57:36 -0700

    started dtree

---
+++ dtree/util.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+from sys import stdout
+
+def iterator_with_progress(n):
+    cursor = '.'
+    last_percent = -1
+    for i in xrange(n):
+        cur_percent = int(100.0 * (i+1) / n)
+        if cur_percent > last_percent:
+            last_percent = cur_percent
+            stdout.write('\r' + cursor * cur_percent + " %d%%" % cur_percent)
+            if cur_percent == 100:
+	            stdout.write('\n')
+            stdout.flush()
+        yield i
